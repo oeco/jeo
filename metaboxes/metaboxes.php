cf59c60b710a55bc2a8fd8718edcc451f9622b96
Miguel Peixe <miguel@cardume.art.br>
2013-02-20 14:48:36 -0300

    use mappress_path on metaboxes include

--- metaboxes/metaboxes.php
+++ metaboxes/metaboxes.php
@@ -5,8 +5,8 @@ add_action('admin_footer', 'mappress_scripts');
 
 /* gather metaboxes */
 
-include(TEMPLATEPATH .  '/inc/mappress/metaboxes/map-relation/map-relation.php');
-include(TEMPLATEPATH .  '/inc/mappress/metaboxes/geocode/geocode.php');
-include(TEMPLATEPATH .  '/inc/mappress/metaboxes/mapbox/mapbox.php');
-include(TEMPLATEPATH .  '/inc/mappress/metaboxes/mapbox/legend.php');
-include(TEMPLATEPATH .  '/inc/mappress/metaboxes/mapgroup/mapgroup.php');
+include(MAPPRESS_PATH .  '/metaboxes/map-relation/map-relation.php');
+include(MAPPRESS_PATH .  '/metaboxes/geocode/geocode.php');
+include(MAPPRESS_PATH .  '/metaboxes/mapbox/mapbox.php');
+include(MAPPRESS_PATH .  '/metaboxes/mapbox/legend.php');
+include(MAPPRESS_PATH .  '/metaboxes/mapgroup/mapgroup.php');
