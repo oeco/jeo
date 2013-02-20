d00ece93b3b7beb7eb675738764cb703f5321ea4
Miguel Peixe <miguel@cardume.art.br>
2013-02-20 15:49:34 -0300

    default map template and featured map function

--- mappress-functions.php
+++ mappress-functions.php
@@ -1,6 +1,24 @@
 <?php
 
 /*
+ * Featured map
+ */
+
+function mappress_featured_map() {
+	$featured_map_id = get_option('mappress_featured_map');
+	if(!$featured_map_id) {
+		$latest_map = get_posts(array('post_type' => array('map', 'map-group'), 'posts_per_page' => 1));
+		if($latest_map) {
+			$latest_map = array_shift($latest_map);
+			$featured_map_id;
+		} else {
+			return false;
+		}
+	}
+	mappress_map($featured_map_id);
+}
+
+/*
  * Display maps
  */
 
@@ -11,9 +29,25 @@ function mappress_map($post_id = false) {
 	$post_id = $post_id ? $post_id : $post->ID;
 	if(!$post_id)
 		return;
+	if(get_post_type($post_id) == 'map-group')
+		return mappress_mapgroup($post_id);
 	setup_postdata(get_post($post_id));
-	get_template_part('content', 'map');
+	if(locate_template('content/map'))
+		get_template_part('content', 'map');
+	else
+		mappress_map_content($post_id);
 	wp_reset_postdata();
+}
+
+function mappress_map_content($post_id = false) {
+	global $post;
+	$post_id = $post_id ? $post_id : $post->ID;
+	if(!$post_id);
+		return;
+	?>
+	<div class="map-container"><div id="map_<?php echo $post->ID; ?>" class="map"></div></div>
+	<script type="text/javascript">mappress(<?php echo $post->ID; ?>);</script>
+	<?php
 }
 
 /*
