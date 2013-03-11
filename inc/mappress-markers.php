e47d6e3c031b3b35214fe67fb0693e019a0176a9
Miguel Peixe <miguel@cardume.art.br>
2013-03-11 03:17:00 -0300

    browser cache

--- inc/mappress-markers.php
+++ inc/mappress-markers.php
@@ -230,10 +230,10 @@ function mappress_get_markers_data($query = false) {
 	header('Content Type: application/json');
 
 	/* Browser caching */
-	//$expires = 60 * 10; // 10 minutes of browser cache
-	//header('Pragma: public');
-	//header('Cache-Control: maxage=' . $expires);
-	//header('Expires: ' . gmdate('D, d M Y H:i:s', time() + $expires) . ' GMT');
+	$expires = 60 * 10; // 10 minutes of browser cache
+	header('Pragma: public');
+	header('Cache-Control: maxage=' . $expires);
+	header('Expires: ' . gmdate('D, d M Y H:i:s', time() + $expires) . ' GMT');
 	/* --------------- */
 
 	echo $data;
