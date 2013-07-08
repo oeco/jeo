c470b999e7baf9add93c7668c95f044b39b82f35
Miguel Peixe <miguel@cardume.art.br>
2013-07-08 11:35:15 -0300

    fix doctype and styles

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ function mappress_theme_scripts() {
 	wp_enqueue_style('mappress-base', get_template_directory_uri() . '/css/base.css', array(), '1.2');
 	wp_enqueue_style('mappress-skeleton', get_template_directory_uri() . '/css/skeleton.css', array('mappress-base'), '1.2');
 	wp_enqueue_style('font-opensans', 'http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800');
-	wp_enqueue_style('mappress-main', get_template_directory_uri() . '/css/main.css', array('mappress-skeleton', 'font-opensans'), '0.0.1.1');
+	wp_enqueue_style('mappress-main', get_template_directory_uri() . '/css/main.css', array('mappress-skeleton', 'font-opensans'), '0.0.2');
 
 	wp_enqueue_script('jquery-isotope', get_template_directory_uri() . '/lib/jquery.isotope.min.js', array('jquery'), '1.5.25');
 
