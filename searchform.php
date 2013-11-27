3a3cfd878ac5531deba2289d14fece74c7d9c758
Vitor George <vitor.george@gmail.com>
2013-11-27 17:22:52 -0200

    Search box translation to Spanish

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" id="searchform">
     <div>
-    	<input type="text" name="s" id="s" placeholder="<?php _e('Search and hit enter', 'jeo'); ?>" value="<?php if(isset($_GET['s'])) echo $_GET['s']; ?>" />
+    	<input type="text" name="s" id="s" placeholder="<?php _e('Search here...', 'jeo'); ?>" value="<?php if(isset($_GET['s'])) echo $_GET['s']; ?>" />
     </div>
 </form>
